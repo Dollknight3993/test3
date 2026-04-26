9ac5cbefb2e17e2e9a851fed6d471a5ff3375438
sinosino <2931357244@qq.com>
2026-04-27 04:07:37 +0800

    Auto stash before merge of "main" and "origin/main"

--- hello.py
+++ hello.py
@@ -2,3 +2,16 @@ def add(x,y):
   return x+y
 
 print("hello world!")
+
+import torch
+import matplotlib.pyplot as plt
+def dm01():
+    t1 = torch.rand(5,3)
+    t2 = torch.sigmoid(t1)
+    plt.imshow(t2.numpy())
+    plt.show()
+
+
+
+if __name__ == '__main__':
+    dm01()
